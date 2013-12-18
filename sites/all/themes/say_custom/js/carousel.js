780961847ffa1d1eae12bd6cf8c06cc59b5d48cd
Chris Morris <cmorris@ccistudios.com>
2013-12-18 12:44:50 -0500

    Fix carousel previous button

--- sites/all/themes/say_custom/js/carousel.js
+++ sites/all/themes/say_custom/js/carousel.js
@@ -40,4 +40,16 @@ var carousel_timer;
 
 		$rows.removeClass("active").filter(".views-row-"+(current+1)).addClass("active");
 	}
+	function previous()
+	{
+		var $rows = $(".view-homepage-carousel .views-row");
+		var current = $rows.filter(".active").index() || 0;
+		var max = $rows.length;
+
+		current--;
+		if (current < 0)
+			current = max-1;
+
+		$rows.removeClass("active").filter(".views-row-"+(current+1)).addClass("active");
+	}
 }(jQuery));
