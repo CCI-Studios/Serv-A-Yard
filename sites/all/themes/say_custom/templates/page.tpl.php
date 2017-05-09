876f4aea1012f7500aac5f6d487800b5b971a2e3
Chris Morris <cmorris@ccistudios.com>
2017-05-09 16:18:45 -0400

    remove service photo sections and move request quote to the sidebar close #28 close #29 close #31

--- sites/all/themes/say_custom/templates/page.tpl.php
+++ sites/all/themes/say_custom/templates/page.tpl.php
@@ -95,20 +95,28 @@
 
     <?php print $messages; ?>
 
-    <div id="main-wrapper"><div id="main" class="clearfix">
+    <div id="main-wrapper" <?php if ($page['sidebar']):?>class="with-sidebar"<?php endif;?>>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <div id="main" class="clearfix">
+      
+        <div id="content" class="column"><div class="section">
+          <a id="main-content"></a>
+          <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <?php print render($page['content']); ?>
+        </div></div> <!-- /.section, /#content -->
+        
+        <?php if ($page['sidebar']): ?>
+          <div class="sidebar"><div>
+            <?php print render($page['sidebar']); ?>
+          </div></div>
+        <?php endif; ?>
 
-      <div id="content" class="column"><div class="section">
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-        <?php print render($page['content']); ?>
-      </div></div> <!-- /.section, /#content -->
-
-    </div></div> <!-- /#main, /#main-wrapper -->
+      </div>
+    </div> <!-- /#main, /#main-wrapper -->
 
     <div id="after-content"><div>
       <?php print render($page['after_content']); ?>
